48467d956452d5eb90ca27ca25294921fc37501e
Jason Dillon <jason@planet57.com>
2014-05-04 21:40:50 -0700

    Move Guice HttpClient provider back, its not api

--- components/nexus-core/src/main/java/org/sonatype/nexus/internal/httpclient/HttpClientProvider.java
+++ components/nexus-core/src/main/java/org/sonatype/nexus/internal/httpclient/HttpClientProvider.java
@@ -10,12 +10,14 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-package org.sonatype.nexus.apachehttpclient;
+package org.sonatype.nexus.internal.httpclient;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Provider;
 import javax.inject.Singleton;
+
+import org.sonatype.nexus.apachehttpclient.Hc4Provider;
 
 import org.apache.http.client.HttpClient;
 
