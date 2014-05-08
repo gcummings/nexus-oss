3e23b1a3030b7915fa70a2d8989aba0af305ae9e
Jason Dillon <jason@planet57.com>
2014-05-07 18:49:51 -0700

    Basic support class for resource

---
+++ plugins/basic/nexus-siesta-plugin/src/main/java/org/sonatype/nexus/plugins/siesta/ResourceSupport.java
@@ -0,0 +1,16 @@
+package org.sonatype.nexus.plugins.siesta;
+
+import org.sonatype.siesta.Resource;
+import org.sonatype.sisu.goodies.common.ComponentSupport;
+
+/**
+ * Support for {@link Resource} implementations.
+ *
+ * @since 3.0
+ */
+public class ResourceSupport
+  extends ComponentSupport
+  implements Resource
+{
+  // empty
+}
